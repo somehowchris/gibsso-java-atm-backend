2f9f55d68b9b4e2d7050345feb336f33fd4d864c
somehowchris <christoftobias.weickhardt@bbzsogr.ch>
2019-12-11 08:56:00 +0100

    Initial commit

---
+++ src/main/java/ch/bbzsogr/Bancomat/BancomatApplication.java
@@ -0,0 +1,13 @@
+package ch.bbzsogr.Bancomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancomatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancomatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbzsogr/Bancomat/BancomatApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbzsogr.Bancomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancomatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
